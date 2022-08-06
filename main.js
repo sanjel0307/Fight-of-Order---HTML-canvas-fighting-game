5a89f56b14d70cf3038f69f47691d13c12928180
sanjel0307 <sanjel0307@gmail.com>
2022-08-06 12:52:17 +0100

    Added Jump and movement mechanics for both players

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ canvas.width = 1280
 canvas.height = 720 
 
 c.fillRect(0, 0, canvas.width, canvas.height)
-const GRAVITY = 0.2 
+const GRAVITY = 0.8
 
 class Sprite { 
     constructor({position, velocity}) { 
@@ -13,6 +13,7 @@ class Sprite {
         this.velocity = velocity
         this.HEIGHT = 150
         this.WIDTH = 50
+        this.lastKey // used to find out the last key used for fluid movement mechanics
     }
 
     draw() {
@@ -23,6 +24,7 @@ class Sprite {
     update() {
         this.draw()
         
+        this.position.x += this.velocity.x 
         this.position.y += this.velocity.y
         
         if (this.position.y + this.HEIGHT + this.velocity.y >= canvas.height) {
@@ -57,6 +59,31 @@ const enemy = new Sprite({
 
 
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowUp: {
+        pressed: false
+    }
+}
+
+ 
+
+
+
 
 function gameLoop() { 
     window.requestAnimationFrame(gameLoop)
@@ -64,7 +91,72 @@ function gameLoop() {
     c.fillRect(0, 0, canvas.width, canvas.height) // clear the canvas every loop frame 
     player.update()
     enemy.update()
+
+    //Player Movement
+    player.velocity.x = 0
+    if (keys.a.pressed && player.lastKey == 'a') {
+        player.velocity.x = -1 
+    } else if (keys.d.pressed && player.lastKey == 'd') {
+        player.velocity.x = 1
+    }
+
+    //Enemy Movement
+    enemy.velocity.x = 0
+    if (keys.ArrowLeft.pressed && enemy.lastKey == 'ArrowLeft') {
+        enemy.velocity.x = -1 
+    } else if (keys.ArrowRight.pressed && enemy.lastKey == 'ArrowRight') {
+        enemy.velocity.x = 1
+    }
 }
 
 gameLoop() //creates an infinite loop of each animation frame 
 
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd': //right P1
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break
+        case 'a': // left P1
+            keys.a.pressed = true 
+            player.lastKey = 'a'
+            break
+        case 'w': // jump P1
+            player.velocity.y = -18
+            break
+        case 'ArrowRight': // right P2 
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft': // left P2 
+            keys.ArrowLeft.pressed = true 
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp': // jump P2
+            enemy.velocity.y = -18
+            break
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd': // right P1 release
+            keys.d.pressed = false
+            break
+        case 'a': // left P1 release
+            keys.a.pressed = false
+            break
+        case 'w': // jump P1 release
+            keys.w.pressed = false
+            break
+        case 'ArrowRight': // right P2 release
+             keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft': // left P2 release
+            keys.ArrowLeft.pressed = false 
+            break
+        case 'ArrowUp': // jump P2 release
+            keys.ArrowUp.pressed = false
+            break
+    }
+})
